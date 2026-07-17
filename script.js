bb14754b80f4f053da8aacf85bd527b2a2c8b4b8
99Nator <rratlholereamogetse@gmail.com>
2026-07-17 11:52:10 +0200

    Move front files to repository root for GitHub Pages

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ const startButton = document.getElementById("start-btn");
 const pauseButton = document.getElementById("pause-btn");
 const resetButton = document.getElementById("reset-btn");
 let seconds = 10;
+const timer = setInterval(countDown, 1000);
 
 function countDown() {
     console.log(seconds);
-    seconds--;
+
+
+if (seconds === 0) {
+    
+   clearInterval(timer);
+    console.log("Time is up!");
+}
+
+seconds--;
 }
 
 function setTimerDisplay(time) {
@@ -27,4 +36,3 @@ resetButton.addEventListener("click", function() {
 
 
 
-setInterval(countDown, 1000);
